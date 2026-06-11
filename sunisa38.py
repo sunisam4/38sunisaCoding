d3d0041b96f8c3829ee6f923ce4c026839e3beba
sunisam4 <27180@bbw.ac.th>
2026-06-11 14:21:44 +0700

    โปรแกรมการคำนวณค่าใช้จ่าย
    เริ่มเข้าใจการเขียนโค้ดมากขึ้นและเข้าใจการใช้คำสั่งมากขึ้น

---
+++ sunisa38.py
@@ -0,0 +1,10 @@
+name = input("คุณชื่ออะไร?")
+age = int(input("คุณอายุเท่าไร "))
+print("สวัสดี" ,name)
+print("อีก1ปี คุณจะอายุ", age +1, "ปี")
+a = 10
+b = 2.5
+c = input("ใส่ข้อความ: ")
+print(type(a))
+print(type(b))
+print(type(c))
