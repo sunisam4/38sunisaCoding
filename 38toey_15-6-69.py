5a8ca61f9f5ba0c72fd841dee9b68a22d6080c0a
sunisam4 <27180@bbw.ac.th>
2026-06-15 09:42:29 +0700

    โปรแกรมเลขคู่หรือเลขขี่
    เรียรู้เกี่ยวกับการใช้คำสั่งหรือตัวแปรต่างๆ

---
+++ 38toey_15-6-69.py
@@ -0,0 +1,8 @@
+number = int(input("Enter your number: "))
+if number %2 == 0:
+    print("Even numbers")
+else:
+    print("odd number")
+print("โปรแกรมเลขคู่หรือเลขขี่")
+print("by toey 4/4")
+print("Tnank you")
